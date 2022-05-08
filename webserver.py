4d8225610201c537ce45d17d07361f5849ce79cd
James Tan <jamestansx@gmail.com>
2022-05-08 20:48:10 +0800

    fixup! Add create data graph function

--- webserver.py
+++ webserver.py
@@ -2,5 +2,4 @@ from webserver import app
 from waitress import serve
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    #serve(app, host='127.0.0.1', port=5000)
+    serve(app, host='127.0.0.1', port=5000)
